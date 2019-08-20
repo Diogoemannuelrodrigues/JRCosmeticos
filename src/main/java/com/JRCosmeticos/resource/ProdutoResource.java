d16ad6ef53416e79a64a0f185c254e2a83784595
Diogo Emannuel <diogoemannuel@MacBook-Pro-de-Diogo.local>
2019-08-20 17:41:55 -0300

    adicionando jackson

--- src/main/java/com/JRCosmeticos/resource/ProdutoResource.java
+++ src/main/java/com/JRCosmeticos/resource/ProdutoResource.java
@@ -1,10 +1,18 @@
 package com.JRCosmeticos.resource;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.JRCosmeticos.model.Produto;
@@ -22,4 +30,14 @@ public class ProdutoResource {
 		return produtoRepository.findAll();
 	}
 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void cradastraProduto(@RequestBody Produto produto) {
+		produtoRepository.save(produto);
+		
+	}
+	@GetMapping("/{id}")
+	public Optional<Produto> buscaPeloId(@PathVariable Long id) {
+		return produtoRepository.findById(id);
+	}
 }
