af56307226725f4e637fb27617e13c1abc0ff1c4
Diogo Emannuel <diogoemannuel@MacBook-Pro-de-Diogo.local>
2019-08-20 18:36:45 -0300

    adicionando query correta 2

--- src/main/resources/db/migration/V02__criando_tabela_pedido.sql
+++ src/main/resources/db/migration/V02__criando_tabela_pedido.sql
@@ -1,7 +1,6 @@
-create table pedido
-(
+create table pedido(
     id integer primary key auto_increment,
     quantidade int not null,
     id_produto integer,
-    constraint fk_id_produto foreing key (id_produto) references produto (id)
+    constraint fk_id_produto foreign key (id_produto) references produto (id)
 );
