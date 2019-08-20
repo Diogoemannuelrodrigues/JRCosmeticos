ce70bbba8e7ea2cb2dde42ba97cda3ac440436ff
Diogo Emannuel <diogoemannuel@MacBook-Pro-de-Diogo.local>
2019-08-20 15:33:13 -0300

    novo projeto

---
+++ src/main/java/com/JRCosmeticos/JrCosmeticosApplication.java
@@ -0,0 +1,13 @@
+package com.JRCosmeticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrCosmeticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrCosmeticosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JRCosmeticos/JrCosmeticosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.JRCosmeticos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JrCosmeticosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
